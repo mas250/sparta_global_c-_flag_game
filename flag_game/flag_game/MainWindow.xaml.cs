b082f4d8ef1b1722a34176159bc5e641afefaa8c
Matthew Shaw <mshaw@sparta.com>
2019-02-25 10:41:29 +0000

    Opens game in new window and places basic layout

---
+++ flag_game/flag_game/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace flag_game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            intro.Text = "*Welcome* \n *Rules*";
+        }
+
+        
+
+        private void launch_easy(object sender, RoutedEventArgs e)
+        {
+            //Window1 myWindow = new Window1();
+            EasyGame eg = new EasyGame();
+            //Window eg = new Window();
+            eg.Show();
+            this.Close();
+        }
+
+    }
+}
